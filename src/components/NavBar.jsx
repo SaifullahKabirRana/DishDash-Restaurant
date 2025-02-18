e5a26b2d8a0103721125fae1076bdc1ae40b2e7d
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2025-02-19 01:44:38 +0600

    added Navbar with responsive

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,107 @@
+import { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+import profile from '../assets/assets/profile.png'
+
+export default function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const navLinks = <>
+        <li><NavLink to='/'>Home</NavLink></li>
+        <li><NavLink to='/contact'>CONTACT us</NavLink></li>
+        <li><NavLink to='/dashboard'>DASHBOARD</NavLink></li>
+        <li><NavLink to='/our-menu'>Our Menu</NavLink></li>
+        <li><NavLink to='/our-shop'>Our Shop</NavLink></li>
+        <li><NavLink to='/login'>SIGN OUT</NavLink></li>
+    </>
+
+    return (
+        <nav className="relative bg-white uppercase ">
+            <div className="container px-6 py-4 mx-auto lg:flex lg:justify-between lg:items-center">
+                {/* Logo */}
+                <div className="flex items-center justify-between">
+                    <div className="flex flex-col  font-cinzel ">
+                        <h2 className=" text-xl md:text-2xl xl:text-3xl font-extrabold ">DishDash</h2>
+                        <p className="text-xs md:text-sm xl:text-lg tracking-[3px] md:tracking-[4px] font-semibold">Restaurant</p>
+                    </div>
+
+                    {/* Mobile Menu Button */}
+                    <div className="flex lg:hidden">
+                        <button
+                            onClick={() => setIsOpen(!isOpen)}
+                            type="button"
+                            className="text-gray-500  hover:text-gray-600 focus:outline-none"
+                            aria-label="toggle menu"
+                        >
+                            {!isOpen ? (
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                                </svg>
+
+                            ) : (
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="w-6 h-6"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                    strokeWidth="2"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        d="M6 18L18 6M6 6l12 12"
+                                    />
+                                </svg>
+                            )}
+                        </button>
+                    </div>
+                </div>
+
+                {/* Menu Items */}
+                <div
+                    className={`absolute inset-x-0 z-50 w-full px-6 py-4 transition-all duration-300 ease-in-out bg-white  lg:mt-0 lg:p-0 lg:top-0 lg:relative lg:bg-transparent lg:w-auto lg:flex lg:items-center ${isOpen
+                        ? "translate-x-0 opacity-100"
+                        : "opacity-0 -translate-x-full lg:opacity-100 lg:translate-x-0"
+                        }`}
+                >
+                    <div  className=" lg:mx-6 ">
+                        <ul
+                        id="nav"
+                        className="flex flex-col lg:flex-row gap-4 my-2 text-gray-700 transition-colors duration-300 transform  lg:mx-6 xl:mx-8 lg:my-0 font-inter font-semibold  ">
+                            {navLinks}
+                        </ul>
+                    </div>
+
+                    {/* Cart Icon */}
+                    <div className="flex justify-center items-center gap-3 ">
+                    <Link
+                            to=''
+                            className="relative text-gray-700 transition-colors duration-300 transform hover:text-gray-600 "
+                        >
+                            <svg
+                                className="w-7 h-7"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    d="M3 3H5L5.4 5M7 13H17L21 5H5.4M7 13L5.4 5M7 13L4.707 15.293C4.077 15.923 4.523 17 5.414 17H17M17 17C15.895 17 15 17.895 15 19C15 20.105 15.895 21 17 21C18.105 21 19 20.105 19 19C19 17.895 18.105 17 17 17ZM9 19C9 20.105 8.105 21 7 21C5.895 21 5 20.105 5 19C5 17.895 5.895 17 7 17C8.105 17 9 17.895 9 19Z"
+                                    stroke="currentColor"
+                                    strokeWidth="2"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                />
+                            </svg>
+
+                            <span className="absolute w-4 h-4 -top-1 -left-1 text-center  text-xs text-white bg-blue-500 rounded-full ">{1}</span>
+                        </Link>
+                        <div>
+                            <img className="w-10" src={profile} alt="" />
+                        </div>
+                       
+                    </div>
+                </div>
+            </div>
+        </nav>
+    );
+}
--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer";
-
+import NavBar from "../components/NavBar";
+import 'flowbite';
 const Main = () => {
     return (
         <div>
+            <NavBar></NavBar>
             <div>
                 <Outlet></Outlet>
             </div>
