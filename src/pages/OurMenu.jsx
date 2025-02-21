c997efe126df128cacd9d4216ba2b69237715f3a
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2025-02-22 03:22:33 +0600

    added react parallex with innerWidth

--- src/pages/OurMenu.jsx
+++ src/pages/OurMenu.jsx
@@ -17,6 +17,36 @@ const OurMenu = () => {
                 upperWord={'uppercase'}
                 ></Cover>
             </div>
+            <div>
+                <Cover
+                bgImg={bgImg}
+                height={'h-[300px] md:h-[420px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]'}
+                paddingY={'py-20 md:py-28 lg:py-36 xl:py-44 2xl:py-48'}
+                heading={'OUR MENU'}
+                subHeading={'Would you like to try a dish?'}
+                upperWord={'uppercase'}
+                ></Cover>
+            </div>
+            <div>
+                <Cover
+                bgImg={bgImg}
+                height={'h-[300px] md:h-[420px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]'}
+                paddingY={'py-20 md:py-28 lg:py-36 xl:py-44 2xl:py-48'}
+                heading={'OUR MENU'}
+                subHeading={'Would you like to try a dish?'}
+                upperWord={'uppercase'}
+                ></Cover>
+            </div>
+            <div>
+                <Cover
+                bgImg={bgImg}
+                height={'h-[300px] md:h-[420px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]'}
+                paddingY={'py-20 md:py-28 lg:py-36 xl:py-44 2xl:py-48'}
+                heading={'OUR MENU'}
+                subHeading={'Would you like to try a dish?'}
+                upperWord={'uppercase'}
+                ></Cover>
+            </div>
         </div>
     );
 };
