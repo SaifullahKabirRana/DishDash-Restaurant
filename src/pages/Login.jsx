dae167977fab4daa348aed2401d050695961bcc7
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2025-02-26 01:58:33 +0600

    added react captcha

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,19 +3,25 @@ import { Link } from 'react-router-dom';
 import bgImg from '../assets/assets/others/authentication.png'
 import loginImg from '../assets/assets/others/authentication2.png'
 import SocialLogin from '../components/SocialLogin';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { VscEye, VscEyeClosed } from 'react-icons/vsc';
+import { loadCaptchaEnginge, LoadCanvasTemplate, LoadCanvasTemplateNoReload, validateCaptcha } from 'react-simple-captcha';
+
 const Login = () => {
     const [showPassword, setShowPassword] = useState(false);
-    
+
+    useEffect(() => {
+        loadCaptchaEnginge(6);
+    }, [])
+
     return (
         <div
             className='h-screen w-full  flex justify-center items-center px-4 md:px-0 '
             style={{ backgroundImage: `url(${bgImg})` }}
         >
-            <div className='shadow-[10px_10px_10px_10px_rgba(0,0,0,0.25)] w-full py-8 md:py-10 md:w-[600px] lg:w-[850px] xl:w-[1200px] 2xl:w-[1400px] md:h-[700px] lg:h-[600px] xl:h-[650px] 2xl:h-[700px]'>
-                <div className='flex flex-col lg:flex-row justify-center lg:gap-0 xl:gap-16 2xl:gap-24 px-4 md:px-0 lg:px-6 xl:px-24 2xl:px-32 lg:py-12 xl:py-16 2xl:py-20 inter'>
-                    <div className='py-4 md:py-6 lg:py-20 flex justify-center '>
+            <div className='shadow-[10px_10px_10px_10px_rgba(0,0,0,0.25)] w-full py-8 lg:py-0 md:w-[600px] lg:w-[850px] xl:w-[1200px] 2xl:w-[1400px]  lg:h-[600px] xl:h-[650px] 2xl:h-[700px]'>
+                <div className='flex flex-col lg:flex-row justify-center lg:gap-0 xl:gap-16 2xl:gap-24 px-4 md:px-0 lg:px-6 xl:px-24 2xl:px-32 lg:py-12 xl:py-12 2xl:py-16 inter'>
+                    <div className='py-2 lg:py-20 xl:py-16 2xl:py-20 flex justify-center '>
                         <img className='w-[270px] md:w-[300px] lg:w-[650px] xl:w-[800px] 2xl:w-[1000px] h-[180px] md:h-[200px] lg:h-[260px] xl:h-[300px] 2xl:h-[350px]' src={loginImg} alt="login" />
                     </div>
                     <div className='w-full md:px-20 lg:px-10 xl:px-14 2xl:px-20'>
@@ -35,7 +41,7 @@ const Login = () => {
                                     className="block w-full px-4 py-2 mt-2 text-gray-700 font-medium bg-white border-[#D0D0D0] rounded-lg  focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40 placeholder:text-sm"
                                 />
                             </div>
-                            <div className="mt-4">
+                            <div className="mt-3 xl:mt-4">
                                 <div className="flex items-center justify-between">
                                     <label
                                         htmlFor="password"
@@ -51,21 +57,33 @@ const Login = () => {
                                     </a>
                                 </div>
                                 <div className='relative flex items-center '>
-                                <input
-                                    type={showPassword? 'text' : 'password'}
-                                    name='password'
-                                    placeholder='Enter your password'
-                                    className="block w-full px-4 py-2 mt-2 text-gray-700 font-medium bg-white border-[#D0D0D0] rounded-lg  focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40 placeholder:text-sm"
-                                />
-                                <span
-                                    className="absolute right-5 pt-1 text-lg md:text-xl "
-                                    onClick={() => setShowPassword(!showPassword)}
-                                >
-                                    {showPassword ? <VscEyeClosed></VscEyeClosed> : <VscEye></VscEye>}
-                                </span>
+                                    <input
+                                        type={showPassword ? 'text' : 'password'}
+                                        name='password'
+                                        placeholder='Enter your password'
+                                        className="block w-full px-4 py-2 mt-2 text-gray-700 font-medium bg-white border-[#D0D0D0] rounded-lg  focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40 placeholder:text-sm"
+                                    />
+                                    <span
+                                        className="absolute right-5 pt-1 text-lg md:text-xl "
+                                        onClick={() => setShowPassword(!showPassword)}
+                                    >
+                                        {showPassword ? <VscEyeClosed></VscEyeClosed> : <VscEye></VscEye>}
+                                    </span>
                                 </div>
                             </div>
-                            <div className="mt-6">
+                            {/* captcha */}
+                            <div className='mt-3 xl:mt-4 block w-full px-4 py-2  text-gray-700 font-medium bg-white border-[#D0D0D0] rounded-lg  focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40 text-sm'>
+                                <LoadCanvasTemplate  />
+                            </div>
+                            <div className='mt-3 xl:mt-4'>
+                                <input
+                                    type="text"
+                                    name='captcha'
+                                    placeholder='Type here'
+                                    className="block w-full px-4 py-2  text-gray-700 font-medium bg-white border-[#D0D0D0] rounded-lg  focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40 placeholder:text-sm"
+                                />
+                            </div>
+                            <div className="mt-5 xl:mt-6">
                                 <input type="submit" value="Sign In"
                                     className="btn w-full  text-sm xl:text-base font-bold tracking-wide text-white capitalize   rounded-lg bg-[#D1A054B3] focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50"
                                 />
@@ -73,8 +91,8 @@ const Login = () => {
                         </form>
 
                         <div className='flex flex-col justify-center items-center pt-4 '>
-                            <Link  to='/register'>
-                            <h2 className='xl:pt-6 text-[#D1A054] text-sm xl:text-base text-center inter font-medium  '>New here? <span className='font-bold  hover:underline active:underline'>Create a New Account</span></h2>
+                            <Link to='/register'>
+                                <h2 className='xl:pt-6 text-[#D1A054] text-sm xl:text-base text-center inter font-medium  '>New here? <span className='font-bold  hover:underline active:underline'>Create a New Account</span></h2>
                             </Link>
                             <p className='text-[#444444] inter font-medium text-sm xl:text-base pt-3 xl:pt-5 text-center'>Or sign in with</p>
                             <SocialLogin></SocialLogin>
