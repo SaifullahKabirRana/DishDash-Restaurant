c7d5af33b4869e3d707203216ca36b1096a3681f
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2025-04-08 04:04:07 +0600

    added bar chat

--- src/pages/Dashboard/AdminHome/AdminHome.jsx
+++ src/pages/Dashboard/AdminHome/AdminHome.jsx
@@ -5,6 +5,11 @@ import { useQuery } from '@tanstack/react-query'
 import { FaTruck, FaUsers } from "react-icons/fa6";
 import { MdMenuBook } from "react-icons/md";
 
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid } from 'recharts';
+
+const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red', 'pink'];
+
+
 const AdminHome = () => {
     const { user } = useAuth();
     const axiosSecure = useAxiosSecure();
@@ -15,9 +20,30 @@ const AdminHome = () => {
             const { data } = await axiosSecure.get(`/admin-stats`);
             return data || [];
         }
+    });
 
-    })
-    
+    const { data: chartData = [] } = useQuery({
+        queryKey: ['order-stats'],
+        queryFn: async () => {
+            const { data } = await axiosSecure.get('/order-stats');
+            return data || [];
+        }
+    });
+
+    // custom shape for the bar chart
+    const getPath = (x, y, width, height) => {
+        return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3}
+        ${x + width / 2}, ${y}
+        C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${x + width}, ${y + height}
+        Z`;
+    };
+
+    const TriangleBar = (props) => {
+        const { fill, x, y, width, height } = props;
+
+        return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+    };
+
     return (
         <div className=" min-h-screen bg-[#F3F3F3]">
             <div className="px-4 md:px-10 lg:px-6 pt-5 md:pt-6 xl:pt-10">
@@ -64,6 +90,31 @@ const AdminHome = () => {
                             </div>
                         </div>
                     </div>
+                </div>
+            </div>
+            {/* bar chart */}
+            <div className="flex">
+                <div className="w-1/2">
+                    <BarChart
+                        width={500}
+                        height={300}
+                        data={chartData}
+                        margin={{
+                            top: 20,
+                            right: 30,
+                            left: 20,
+                            bottom: 5,
+                        }}
+                    >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="category" />
+                        <YAxis />
+                        <Bar dataKey="quantity" fill="#8884d8" shape={<TriangleBar />} label={{ position: 'top' }}>
+                            {chartData.map((entry, index) => (
+                                <Cell key={`cell-${index}`} fill={colors[index % 6]} />
+                            ))}
+                        </Bar>
+                    </BarChart>
                 </div>
             </div>
         </div>
