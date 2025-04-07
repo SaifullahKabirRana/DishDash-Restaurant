13d548ed4158179d2b46a6902e147eb829eb13e5
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2025-04-07 20:26:12 +0600

    added scroll to top

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -2,11 +2,13 @@ import { Outlet, useLocation } from "react-router-dom";
 import Footer from "../components/Footer";
 import NavBar from "../components/NavBar";
 import 'flowbite';
+import ScrollToTop from "../components/ScrollToTop";
 const Main = () => {
     const location = useLocation();
     const noNavbarFooter = location.pathname.includes('login') || location.pathname.includes('register');
     return (
         <div>
+            <ScrollToTop></ScrollToTop>
             {noNavbarFooter || <NavBar></NavBar>}
             <Outlet></Outlet>
             {noNavbarFooter || <Footer></Footer>}
